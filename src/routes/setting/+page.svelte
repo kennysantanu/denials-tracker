7e187a4d33f9ae32ffe6b183c5b90909b3ddc608
Kenny Santanu <31720797+kennysantanu@users.noreply.github.com>
2024-06-09 01:18:51 -0700

    feat: Add setting page

---
+++ src/routes/setting/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-col items-center">
+	<!-- Page Content -->
+	<div class="flex w-full max-w-7xl flex-col items-center space-y-16 px-4">
+
+		<!-- Setting Card -->
+		<div class="card w-full space-y-12 px-8 py-8">
+			<div class="flex flex-row space-x-16">
+				<!-- Setting Sidebar Card -->
+				<div class="card space-y-8 px-4 py-4 w-96 basis-1/4">
+					<div class="space-y-4">
+						<h4 class="text-tertiary-500">Group Setting</h4>
+						<ul class="space-y-4">
+							<li>- Setting</li>
+						</ul>
+						<ul class="space-y-4">
+							<li>- Setting</li>
+						</ul>
+						<ul class="space-y-4">
+							<li>- Setting</li>
+						</ul>
+					</div>
+					<div class="space-y-4">
+						<h4 class="text-tertiary-500">Group Setting</h4>
+						<ul class="space-y-4">
+							<li>- Setting</li>
+						</ul>
+						<ul class="space-y-4">
+							<li>- Setting</li>
+						</ul>
+						<ul class="space-y-4">
+							<li>- Setting</li>
+						</ul>
+					</div>
+					<div class="space-y-4">
+						<h4 class="text-tertiary-500">Group Setting</h4>
+						<ul class="space-y-4">
+							<li>- Setting</li>
+						</ul>
+						<ul class="space-y-4">
+							<li>- Setting</li>
+						</ul>
+						<ul class="space-y-4">
+							<li>- Setting</li>
+						</ul>
+					</div>
+				</div>
+
+				<!-- Setting Contents -->
+				<div class="flex-col space-y-8">
+					<label>
+						<span class="text-tertiary-500">Setting 1</span>
+						<input class="input" type="text"/>
+					</label>
+					<label>
+						<span class="text-tertiary-500">Setting 2</span>
+						<input class="input" type="text"/>
+					</label>
+					<label>
+						<span class="text-tertiary-500">Setting 3</span>
+						<input class="input" type="text"/>
+					</label>
+
+					<button type="button" class="variant-filled-primary btn">Button</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
