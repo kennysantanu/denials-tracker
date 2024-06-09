b5f0d276b6de475101dbb9ba76099f4e42829f4a
Kenny Santanu <31720797+kennysantanu@users.noreply.github.com>
2024-06-09 01:16:24 -0700

    feat: Add Header and Navbar components to layout

---
+++ src/lib/Header.svelte
@@ -0,0 +1,8 @@
+<div
+	class="flex w-full flex-row items-center justify-between bg-slate-200 px-24 py-3 drop-shadow-md"
+>
+	<h3 class="h3 font-bold text-tertiary-500">Denials Tracker</h3>
+	<div class="flex flex-row space-x-4">
+		<button type="button" class="variant-filled-primary btn">Sign in</button>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
 	import '../app.css';
+	import Header from '$lib/Header.svelte';
+	import Navbar from '$lib/Navbar.svelte';	
 </script>
 
+<Header />
+<Navbar />
 <slot></slot>
 
 <style></style>
