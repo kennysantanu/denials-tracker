5dcc5a0818d04391361a4ac66493c3749f744d82
Kenny Santanu <31720797+kennysantanu@users.noreply.github.com>
2024-07-05 02:43:23 -0700

    feat: edit user role

--- src/routes/(app)/setting/admin/roles/+page.server.ts
+++ src/routes/(app)/setting/admin/roles/+page.server.ts
@@ -4,41 +4,63 @@ import { z } from 'zod';
 import type { PageServerLoad, Actions } from './$types';
 
 // Form schema
-const schemaNewRoleForm = z.object({
+const rolesSchema = z.object({
+    id: z.number(),
     role_name: z.string(),
 });
 
 // Server load function
 export const load = (async ({ locals: { supabase, safeGetSession } }) => {
-    const newRoleForm = await superValidate(zod(schemaNewRoleForm));
+    const newRoleForm = await superValidate(zod(rolesSchema));
+    const editRoleForm = await superValidate(zod(rolesSchema));
     
-    let { data: roles, error } = await supabase
+    let { data: roles } = await supabase
     .from('roles')
-    .select('*')        
+    .select('*') 
+    .order('id', { ascending: true })       
 
-    return { newRoleForm, roles: roles || []};
+    return { newRoleForm, editRoleForm, roles: roles || []};
 }) satisfies PageServerLoad;
 
 // Form actions
 export const actions: Actions = {
     createRole: async ({ request, locals: { supabase, safeGetSession } }) => {
-        const form = await superValidate(request, zod(schemaNewRoleForm));
+        const newRoleForm = await superValidate(request, zod(rolesSchema));
 
-        if (!form.valid) {
-            return fail(400, { form });
+        if (!newRoleForm.valid) {
+            return fail(400, { newRoleForm });
         }
 
         const { data, error } = await supabase
             .from('roles')
             .insert([
-            { role_name: form.data.role_name.trim() },
-            ])
-            .select()
-                      
+            { role_name: newRoleForm.data.role_name.trim() },
+            ]);
+
         if (error) {
-            return fail(400, { form });
+            return fail(400, { newRoleForm });
         }
 
-        return form;
+        return { newRoleForm };
+    },
+    updateRole: async ({ request, locals: { supabase, safeGetSession } }) => {
+        const editRoleForm = await superValidate(request, zod(rolesSchema));
+
+        if (!editRoleForm.valid) {
+            return fail(400, { editRoleForm });
+        }
+        
+        const { error } = await supabase
+            .from('roles')
+            .update({
+                role_name: editRoleForm.data.role_name.trim(),
+            })
+            .eq('id', editRoleForm.data.id)
+
+        if (error) {
+            return fail(400, { editRoleForm });
+        }
+
+        return { editRoleForm };
     },
 };
--- src/routes/(app)/setting/admin/roles/+page.svelte
+++ src/routes/(app)/setting/admin/roles/+page.svelte
@@ -5,7 +5,21 @@
 	export let data: PageData;
 
 	// SuperForm forms
-	const { form: newRoleForm, constraints: newRoleFormConstraints } = superForm(data.newRoleForm);
+	const {
+		form: newRoleForm,
+		constraints: newRoleFormConstraints,
+		enhance: newRoleFormEnhanced
+	} = superForm(data.newRoleForm, {
+		id: 'createRoleForm'
+	});
+
+	const {
+		form: editRoleForm,
+		constraints: editRoleFormConstraints,
+		enhance: editRoleFormEnhanced
+	} = superForm(data.editRoleForm, {
+		id: 'editRoleForm'
+	});
 </script>
 
 <h2 class="h2 text-tertiary-500">List of Roles</h2>
@@ -17,7 +31,13 @@
 
 <hr />
 
-<form action="?/createRole" method="post" class="space-y-4">
+<form
+	action="?/createRole"
+	method="post"
+	id="createRoleForm"
+	class="space-y-4"
+	use:newRoleFormEnhanced
+>
 	<h2 class="h2 text-tertiary-500">Create New Role</h2>
 	<label class="label">
 		<span class="text-tertiary-500">Role Name</span>
@@ -31,5 +51,38 @@
 	</label>
 	<div class="space-x-4">
 		<button type="submit" class="variant-filled-primary btn">Create Role</button>
+	</div>
+</form>
+
+<hr />
+
+<form
+	action="?/updateRole"
+	method="post"
+	id="editRoleForm"
+	class="space-y-4"
+	use:editRoleFormEnhanced
+>
+	<h2 class="h2 text-tertiary-500">Edit Role</h2>
+	<label class="label">
+		<span class="text-tertiary-500">Role Select</span>
+		<select class="select" name="id" bind:value={$editRoleForm.id} {...$editRoleFormConstraints.id}>
+			{#each data.roles as role}
+				<option value={role.id}>{role.role_name}</option>
+			{/each}
+		</select>
+	</label>
+	<label class="label">
+		<span class="text-tertiary-500">Role Name</span>
+		<input
+			type="text"
+			name="role_name"
+			class="input"
+			bind:value={$editRoleForm.role_name}
+			{...$editRoleFormConstraints.role_name}
+		/>
+	</label>
+	<div class="space-x-4">
+		<button type="submit" class="variant-filled-primary btn">Edit Role</button>
 	</div>
 </form>
