b1a256571b7f20d686f9e427981f66a1280cbfd7
kennysantanu <31720797+kennysantanu@users.noreply.github.com>
2025-05-30 17:14:28 -0700

    feat: Refactor navigation header to use AppBar component for improved layout and navigation

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { TabGroup, TabAnchor, storePopup } from '@skeletonlabs/skeleton';
+	import { AppBar, TabGroup, TabAnchor, storePopup } from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 
 	export let data;
@@ -8,46 +8,55 @@
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 </script>
 
-<!-- Header Bar -->
-<div class="flex items-center justify-between bg-slate-200 px-24 py-3 drop-shadow-md">
-	<h3 class="h3 font-bold text-tertiary-500">Denials Tracker</h3>
-	{#if !data.user?.username}
-		<div class="flex flex-row space-x-4">
-			<a href="/signin" class="variant-filled-primary btn">Sign in</a>
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">
+		<div class="space-x-2 lg:hidden">
+			<a href="/record">Record</a>
+			<a href="/report">Report</a>
+			<a href="/file">File</a>
+			<a href="/setting">Setting</a>
 		</div>
-	{:else}
-		<div class="flex flex-row items-center space-x-4">
-			<p>{data.user.username}</p>
+		<h3 class="h3 hidden font-bold text-tertiary-500 lg:inline">Denials Tracker</h3>
+	</svelte:fragment>
+	<h3 class="h3 text-center font-bold text-tertiary-500 lg:hidden">Denials Tracker</h3>
+	<div class="hidden flex-col items-center px-4 lg:flex">
+		<div class="w-fit">
+			<TabGroup
+				justify="justify-center space-x-16"
+				active="border-b-2 border-tertiary-500 text-tertiary-500"
+				hover="hover:variant-soft-primary"
+			>
+				<TabAnchor href="/record" selected={$page.url.pathname.startsWith('/record')}
+					>Record</TabAnchor
+				>
+				<TabAnchor href="/report" selected={$page.url.pathname.startsWith('/report')}
+					>Report</TabAnchor
+				>
+				<TabAnchor href="/file" selected={$page.url.pathname.startsWith('/file')}>File</TabAnchor>
+				<TabAnchor href="/setting" selected={$page.url.pathname.startsWith('/setting')}
+					>Setting</TabAnchor
+				>
+			</TabGroup>
+		</div>
+	</div>
+	<svelte:fragment slot="trail">
+		{#if !data.user?.username}
+			<div class="flex flex-row space-x-4">
+				<a href="/signin" class="variant-filled-primary btn">Sign in</a>
+			</div>
+		{:else}
+			<div class="hidden flex-row items-center space-x-4 lg:flex">
+				<p>{data.user.username}</p>
+			</div>
 			<form method="POST" action="/signin?/signout">
 				<button type="submit" class="variant-filled-primary btn">Sign out</button>
 			</form>
-		</div>
-	{/if}
-</div>
+		{/if}
+	</svelte:fragment>
+</AppBar>
 
 <div class="flex flex-col items-center">
 	<div class="w-full max-w-7xl">
-		<!-- Navigation Bar -->
-		<div class="flex flex-col items-center px-4 py-16">
-			<div class="w-fit">
-				<TabGroup
-					justify="justify-center space-x-16"
-					active="border-b-2 border-tertiary-500 text-tertiary-500"
-					hover="hover:variant-soft-primary"
-				>
-					<TabAnchor href="/record" selected={$page.url.pathname.startsWith('/record')}
-						>Record</TabAnchor
-					>
-					<TabAnchor href="/report" selected={$page.url.pathname.startsWith('/report')}
-						>Report</TabAnchor
-					>
-					<TabAnchor href="/file" selected={$page.url.pathname.startsWith('/file')}>File</TabAnchor>
-					<TabAnchor href="/setting" selected={$page.url.pathname.startsWith('/setting')}
-						>Setting</TabAnchor
-					>
-				</TabGroup>
-			</div>
-		</div>
 		<div class="m-2 space-y-8">
 			<slot></slot>
 		</div>
